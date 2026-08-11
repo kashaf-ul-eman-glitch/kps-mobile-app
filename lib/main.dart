4584a38399f2ba46349e483df9ee15a46985162b
Kashaf ul Eman <huzaifakkhan246@gmail.com>
2026-08-11 12:47:29 +0500

    trying to connect with firebase abd updated main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'screens/splash_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -12,7 +18,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'School App',
+      title: 'Small App Flutter',
+      theme: ThemeData(
+        primarySwatch: Colors.brown,
+      ),
       home: const SplashScreen(),
     );
   }
